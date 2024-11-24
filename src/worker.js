79359887b67b43a104694b5f00d002f621b2bcb5
Mehdi Askari <me@mehdiaskari.ir>
2024-11-24 18:29:59 +0330

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,8 +6,8 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
-let trojanPassword = `bpb-trojan`;
+let userID = '8899c602-e969-45ff-9be8-e6aad3b045eb';
+let trojanPassword = `mxsrv`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
